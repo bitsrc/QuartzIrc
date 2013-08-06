058ffa36494ace0e27c1f6a10ca6b8331c68b585
phate408 <phate408@gmail.com>
2013-08-06 00:35:18 -0700

    Added TestClient project

---
+++ TestClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using QuartzIrc;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
