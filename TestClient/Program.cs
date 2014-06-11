bceb01c54ea9bfd81ec342a51879cd95edcf6727
phate408 <phate408@gmail.com>
2014-06-11 04:37:57 -0700

    Fleshed out test client

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 using QuartzIrc;
 
@@ -11,6 +12,19 @@ namespace TestClient
     {
         static void Main(string[] args)
         {
+            TestClient client = new TestClient();
+            Thread ircThread = new Thread(client.Start);
+            ircThread.Start();
+            while (true)
+            {
+                String input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    client.Quit();
+                    break;
+                }
+                client.Send(input);
+            }
         }
     }
 }
---
+++ TestClient/TestClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using QuartzIrc;
+
+namespace TestClient
+{
+    class TestClient
+    {
+        IrcClient Client;
+
+        public TestClient()
+        {
+            IrcConfig config = new IrcConfig("irc.seersirc.net","TestClient");
+            config.Port = 6697;
+            config.Ssl = true;
+            config.Realname = "QuartzIrc Framework - Phate408";
+
+            Client = new IrcClient(config);
+
+            Client.EventRawIn += new IrcRawEventHandler(OnRaw);
+        }
+
+        public void Start()
+        {
+            Client.Start();
+        }
+
+        public void Send(string s) {
+            Client.Raw(s);
+        }
+
+        public void Quit()
+        {
+            Client.Quit();
+        }
+
+        public void OnRaw(IrcClient sender, String text)
+        {
+            Console.WriteLine("{0}", text);
+        }
+    }
+}
